186f35b580247e76a5b6313e8b3c44457306a1af
ulisses bertoldi <ulissesbertoldi@hotmail.com>
2017-07-19 21:41:59 -0300

    todos

---
+++ src/main/java/com/performance/GeneralArguments.java
@@ -0,0 +1,8 @@
+package com.performance;
+
+public class GeneralArguments {
+	public static String serverIP = "192.168.1.200";
+	public static int cacheSize = -1; // caxhe size
+	//public static int agesBound = 40; // variation size data
+	//public static int age = 50; // size data
+}
